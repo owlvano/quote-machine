5b209f0d72ed1faae83dde6f5e6712263fa757b1
Ivan Sova <owlvano@gmail.com>
2020-05-06 20:22:35 +0300

    upload initial project from codepen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tweetIntent = (quote) => {
+    const tweetText = encodeURI(quote.text + '\n- ' + quote.author);
+    return `https://twitter.com/intent/tweet?text=${tweetText}`;
+};
+
+const randomItem = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const quotes = [
+    {
+        text: 'Когда я родился - участковый застрелился.',
+        author: 'Джейсон Стэтхэм',
+    },
+    {
+        text: 'Не слушай тех, кто много обещают, они обычно много обещают.',
+        author: 'Волчара',
+    },
+    {
+        text: "Oh, so you're a fan of know Beethoven? Name 5 of his albums!",
+        author: 'Сидоджи',
+    },
+];
+
+const Quote = ({ quote: { text, author } }) => (
+    <>
+        <div id="text">{text}</div>
+        <div id="author">- {author}</div>
+    </>
+);
+
+const QuoteMachine = () => {
+    const [quote, setQuote] = React.useState(randomItem(quotes));
+
+    const setNewQuote = () => {
+        setQuote(randomItem(quotes));
+    }
+
+    return (
+        <div id="quote-box">
+            <Quote quote={quote} />
+
+            <div id="button-box">
+                <button id="new-quote" onClick={setNewQuote}>Новая цитата</button>
+                <a href={tweetIntent(quote)} id="tweet-quote">Твитнуть цитату</a>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default QuoteMachine;
