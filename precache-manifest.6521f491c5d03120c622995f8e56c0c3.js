aee69ccf7b362e9079e0974c7c04689bbad34cb4
Ivan Sova <owlvano@gmail.com>
2020-05-20 17:55:04 +0300

    Updates

---
+++ precache-manifest.6521f491c5d03120c622995f8e56c0c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cc33f6e61e88ce8b25aa5a4ffa28c7",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "a903a5883b87da39fb1e",
+    "url": "/quote-machine/static/css/main.3e128310.chunk.css"
+  },
+  {
+    "revision": "73a5dcf39cf79e3f44b4",
+    "url": "/quote-machine/static/js/2.5ef00974.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.5ef00974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a903a5883b87da39fb1e",
+    "url": "/quote-machine/static/js/main.94a8e500.chunk.js"
+  },
+  {
+    "revision": "9ba28ae8763e5db583e7",
+    "url": "/quote-machine/static/js/runtime-main.4b07b1f0.js"
+  },
+  {
+    "revision": "7f8e365b9eda150ba8edd8aae929a3d0",
+    "url": "/quote-machine/static/media/twitter.7f8e365b.svg"
+  }
+]);
